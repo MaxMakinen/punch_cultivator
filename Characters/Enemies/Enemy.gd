75ed302e0da1e3e4ee820fe95cffd2baad50bf8b
Max Mäkinen <quantumhound@gmail.com>
2024-05-07 10:21:23 +0300

    Adding player targeting movement

--- Characters/Enemies/Enemy.gd
+++ Characters/Enemies/Enemy.gd
@@ -4,25 +4,15 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+var target: CharacterBody2D = self
 
+func set_target(new_target: CharacterBody2D) -> void:
+	target = new_target
+
+func _choose_direction() -> void:
+	var direction: Vector2 = target.position - self.position
+	velocity = direction.normalized() * SPEED
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	_choose_direction()
 	move_and_slide()
