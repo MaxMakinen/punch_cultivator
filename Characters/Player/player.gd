f5accf43ea1a1b3d8ee4c3dc43769324b060ed8e
Max Mäkinen <quantumhound@gmail.com>
2024-05-08 14:58:42 +0300

    testing different position info

--- Characters/Player/player.gd
+++ Characters/Player/player.gd
@@ -23,7 +23,7 @@ func _on_hurt_box_area_entered(area: Area2D) -> void:
 
 func _on_punch_box_area_entered(area: Area2D) -> void:
 	var punch = PUNCHPLOSION.instantiate()
-	punch.position = area.position
+	punch.position = area.global_position
 	add_child(punch)
 
 
