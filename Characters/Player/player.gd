0709cb7dc507a300f59d38f8ff62c914fa48b8f6
Max Mäkinen <quantumhound@gmail.com>
2024-05-08 14:39:56 +0300

    testing punch area reacting to enemies

--- Characters/Player/player.gd
+++ Characters/Player/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @export var speed = 400
 @onready var hurt_box: Area2D = $HurtBox
 
+const PUNCHPLOSION = preload("res://attacks/punchplosion.tscn")
 
 func _get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
@@ -18,4 +19,15 @@ func _physics_process(_delta: float) -> void:
 
 func _on_hurt_box_area_entered(area: Area2D) -> void:
 	print("PAIN")
+	pass # Replace with function body.
+
+
+func _on_punch_box_area_entered(area: Area2D) -> void:
+	var punch = PUNCHPLOSION.instantiate()
+	punch.position = area.position
+	add_child(punch)
+
+
+
+func _on_punch_box_area_exited(area: Area2D) -> void:
 	pass # Replace with function body.
