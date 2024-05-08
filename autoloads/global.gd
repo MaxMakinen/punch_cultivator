da17fe796ae2d9daa31145e8b93566e87ef25015
Max Mäkinen <quantumhound@gmail.com>
2024-05-08 15:35:15 +0300

    Adding stuff to autoload

--- autoloads/global.gd
+++ autoloads/global.gd
@@ -5,4 +5,9 @@ const PUNCHPLOSION = preload("res://attacks/punchplosion.tscn")
 
 var difficulty: int = 0
 
+var punch: Dictionary = {
+	"name" : "punch",
+	"damage" : 5,
+	"scene" : PUNCHPLOSION
+}
 
