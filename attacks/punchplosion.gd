dbd4eb04ba58f830db0cfa668560aa46ba11c05f
Max Mäkinen <quantumhound@gmail.com>
2024-05-08 15:35:34 +0300

    Made explosion bigger

--- attacks/punchplosion.gd
+++ attacks/punchplosion.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
+var target_pos: Vector2 = Vector2.ZERO
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("PUNCH!")
+	self.position = target_pos
 	$CPUParticles2D.emitting = true
 	pass # Replace with function body.
 
@@ -13,3 +16,5 @@ func _process(delta: float) -> void:
 	if $CPUParticles2D.emitting == false:
 		queue_free()
 
+func set_pos(new_pos: Vector2) -> void:
+	target_pos = new_pos
