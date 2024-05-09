b8ae7598ab4d719d5bde93076536bc342c7004b7
Max Mäkinen <quantumhound@gmail.com>
2024-05-09 12:52:34 +0300

    Testing weapon that autoshoots at nearest enemy

---
+++ attacks/weapons/weapon.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+
+@onready var cooldown_timer: Timer = $CooldownTimer
+@onready var muzzle: Marker2D = $Muzzle
+@export var projectile: PackedScene
+@export var cooldown: float = 0.2
+
+var shot: bool = false
+var dir: Vector2 = Vector2.ZERO
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	get_parent().direction_changed.connect(_aim)
+	cooldown_timer.wait_time = cooldown
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	await get_tree().create_timer(0.5).timeout
+	shoot()
+	pass
+
+func _aim(direction: Vector2) -> void:
+	dir = direction.normalized()
+
+func shoot() -> void:
+	if cooldown_timer.is_stopped():
+		var b = projectile.instantiate()
+		look_at(self.global_position + dir)
+		get_parent().get_parent().add_child(b)
+		b.transform = muzzle.global_transform
+		cooldown_timer.start(cooldown)
---
+++ attacks/weapons/weapon_detecting.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+
+@onready var cooldown_timer: Timer = $CooldownTimer
+@onready var muzzle: Marker2D = $Muzzle
+@export var projectile: PackedScene
+@export var cooldown: float = 0.2
+
+var shot: bool = false
+var dir: Vector2 = Vector2.ZERO
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	cooldown_timer.wait_time = cooldown
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _aim(direction: Vector2) -> void:
+	dir = direction.normalized()
+
+func shoot() -> void:
+	if cooldown_timer.is_stopped():
+		var b = projectile.instantiate()
+		look_at(self.global_position + dir)
+		get_parent().get_parent().add_child(b)
+		b.transform = muzzle.global_transform
+		cooldown_timer.start(cooldown)
+
+
+func _on_target_detection_body_entered(body: Node2D) -> void:
+	if body.is_in_group("enemy"):
+		_aim(body.position - self.position)
+		shoot()
+
