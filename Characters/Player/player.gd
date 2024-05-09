f857caace61f7466d0e255148fc3b46a02cb5298
Max Mäkinen <quantumhound@gmail.com>
2024-05-09 12:53:12 +0300

    Adding vars into exports

--- Characters/Player/player.gd
+++ Characters/Player/player.gd
@@ -4,11 +4,30 @@ extends CharacterBody2D
 @export var speed = 400
 @onready var hurt_box: Area2D = $HurtBox
 
-const PUNCHPLOSION = preload("res://attacks/punchplosion.tscn")
+@onready var muzzle: Marker2D = $Muzzle
+
+@export var Bullet : PackedScene
+@export var Weapon : PackedScene
+
+
+var shot : bool = false
+var dir = Vector2.ZERO
+var weapon
+
+signal direction_changed(dir)
+
+func _ready() -> void:
+	weapon = Weapon.instantiate()
+	add_child(weapon)
+
 
 func _get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
+	if input_direction != Vector2.ZERO:
+		dir = input_direction.normalized()
+		#weapon.aim(dir)
+		direction_changed.emit(dir)
 
 
 func _physics_process(_delta: float) -> void:
@@ -24,8 +43,3 @@ func _on_hurt_box_area_entered(area: Area2D) -> void:
 func _on_punch_box_area_entered(area: Area2D) -> void:
 	if area.owner != self:
 		area.owner.get_attacked(Global.punch)
-
-
-
-func _on_punch_box_area_exited(area: Area2D) -> void:
-	pass # Replace with function body.
