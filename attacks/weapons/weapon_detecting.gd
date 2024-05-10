9449d0269ec629b8b52cf39ebea7e26ad3856d1d
Max Mäkinen <quantumhound@gmail.com>
2024-05-10 15:17:46 +0300

    removed unused code

--- attacks/weapons/weapon_detecting.gd
+++ attacks/weapons/weapon_detecting.gd
@@ -15,12 +15,17 @@ var valid_targets: Array[Node2D] = []
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	cooldown_timer.wait_time = cooldown
-	pass # Replace with function body.
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+	if enemy_in_attack_zone == true:
+		if cooldown_timer.is_stopped():
+			for target in valid_targets:
+				_aim(target.position)
+				shoot()
+
 
 
 func _aim(direction: Vector2) -> void:
@@ -44,16 +49,26 @@ func get_max_cooldown() -> float:
 
 func _add_enemy_to_targets(enemy: Node2D) -> void:
 	valid_targets.append(enemy)
+	enemy.enemy_dead.connect(_remove_from_targets)
+
+
+func _remove_from_targets(enemy: Node2D) -> void:
+	if enemy in valid_targets:
+		valid_targets.erase(enemy)
+		if enemy.is_connected("enemy_dead", _remove_from_targets):
+			enemy.enemy_dead.disconnect(_remove_from_targets)
 
 
 func _on_target_detection_body_entered(body: Node2D) -> void:
 	if body.is_in_group("enemy"):
 		enemy_in_attack_zone = true
-		_aim(body.global_position)
-		shoot()
+		_add_enemy_to_targets(body)
+
 
 
 func _on_target_detection_body_exited(body: Node2D) -> void:
 	if body.is_in_group("enemy"):
-		enemy_in_attack_zone = false
+		_remove_from_targets(body)
+		if valid_targets.is_empty():
+			enemy_in_attack_zone = false
 		
