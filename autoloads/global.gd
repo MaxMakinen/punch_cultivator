b99443d806d516de86b03897f4fdd5d8a785946d
Max Mäkinen <quantumhound@gmail.com>
2024-05-13 12:12:23 +0300

    renamed collsion layer

--- autoloads/global.gd
+++ autoloads/global.gd
@@ -1,10 +1,11 @@
 extends Node
 
-
+const EXP_NODE = preload("res://pickups/exp_node.tscn")
 const PUNCHPLOSION = preload("res://attacks/punchplosion.tscn")
 
 var difficulty: int = 0
 var player_health: int = 10
+var experience: int = 0
 
 var punch: Dictionary = {
 	"name" : "punch",
@@ -12,3 +13,13 @@ var punch: Dictionary = {
 	"scene" : PUNCHPLOSION
 }
 
+func gain_exp(new_exp: int) -> void:
+	experience += new_exp
+
+func get_exp() -> int:
+	return experience
+
+func spawn_exp(pos: Vector2, value: int) -> Node2D:
+	var exp_node = EXP_NODE.instantiate()
+	exp_node.initialize(pos, value)
+	return exp_node
