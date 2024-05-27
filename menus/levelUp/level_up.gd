b26f7685cff1fb277a7e4bb9ccf9327a49dcd9c3
Max Mäkinen <quantumhound@gmail.com>
2024-05-27 14:22:40 +0300

    Added more buttons for more update options

--- menus/levelUp/level_up.gd
+++ menus/levelUp/level_up.gd
@@ -2,9 +2,12 @@ extends ColorRect
 
 @export var new_weapon: PackedScene
 var check: bool = true
+var player
+
+func _ready() -> void:
+	player = get_tree().get_first_node_in_group("player")
 
 func _on_button_pressed() -> void:
-	var player = get_tree().get_first_node_in_group("player")
 	if !check:
 		player.equip_weapon(new_weapon)
 		check = true
@@ -16,3 +19,26 @@ func _on_button_pressed() -> void:
 func _unpause() -> void:
 	owner.get_tree().paused = false
 	hide()
+
+
+func _on_upgrade_cooldown_pressed() -> void:
+	player.get_weapun().upgrade_cooldown()
+	_unpause()
+
+func _on_upgrade_speed_pressed() -> void:
+	player.get_weapun().upgrade_speed()
+	_unpause()
+
+
+func _on_upgrade_combo_pressed() -> void:
+	player.get_weapun().upgrade_combo()
+	_unpause()
+
+
+func _on_upgrade_combo_speed_pressed() -> void:
+	player.get_weapun().upgrade_combo_cooldown()
+	_unpause()
+	
+
+#func upgrade_range(range_increase: float = 0.1) -> void:
+#	attack["range"] += range_increase
