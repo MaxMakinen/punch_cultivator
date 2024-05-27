4eea6eee9963cd92c364da48a885c42acfae7321
Max Mäkinen <quantumhound@gmail.com>
2024-05-27 14:24:08 +0300

    Added max amount of enemies on map

--- Maps/EnemySpawner/enemy_spawner.gd
+++ Maps/EnemySpawner/enemy_spawner.gd
@@ -13,7 +13,8 @@ extends Node2D
 # VARIABLES
 var spawn_wave_size: int = 1
 var spawning: bool = false
-
+var max_enemy_amount: int = 10
+var enemy_amount: int = 0
 
 # PRIVATE FUNCTIONS
 # Called when the node enters the scene tree for the first time.
@@ -30,12 +31,15 @@ func _spawn_wave() -> void:
 	# Create a new instance of our hostile entity.
 	var wave: Array[CharacterBody2D] = []
 	for index in spawn_wave_size:
-		wave.append(hostile.instantiate())
+		if enemy_amount < max_enemy_amount:
+			enemy_amount += 1
+			wave.append(hostile.instantiate())
 	for mob in wave:
 		# Give it a random offset
 		spawn_location.progress_ratio = randf()
 		mob.initialize(player, spawn_location.global_position)
 		enemy_container.add_child(mob)
+		mob.enemy_dead.connect(_remove_enemy)
 	# Increase the size of nect wave
 	_spawn_wave_increase()
 
@@ -43,6 +47,10 @@ func _spawn_wave() -> void:
 func _spawn_wave_increase() -> void:
 	if spawn_wave_size < 10:
 		spawn_wave_size += 1
+
+
+func _remove_enemy(_enemy: Node2D) -> void:
+	enemy_amount -= 1
 
 # PUBLIC FUNCTIONS
 func get_wave_size() -> int:
