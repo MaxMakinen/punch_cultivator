dbe9892ce870dbbf49239f993d885ce2244c217b
Max Mäkinen <quantumhound@gmail.com>
2024-06-03 12:42:23 +0300

    removed unused lines

--- autoloads/EnemyData.gd
+++ autoloads/EnemyData.gd
@@ -20,12 +20,6 @@ var canine: Dictionary = {
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_enemy_list["canine"] = canine
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 
 # Return enemy dict for apprpriate danger level
