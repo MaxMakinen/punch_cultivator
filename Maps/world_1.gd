fe4e4d285c2498516424f1440127e05edbc24f6a
Max Mäkinen <quantumhound@gmail.com>
2024-06-03 14:23:35 +0300

    game now starts in main menu

--- Maps/world_1.gd
+++ Maps/world_1.gd
@@ -16,7 +16,7 @@ extends Node2D
 
 @onready var player: CharacterBody2D = $Player
 
-@export var time: int = 30
+@export var time: int = 5
 
 @onready var cooldown: Label = $Camera2D/UIRoot/Cooldown
 @onready var cooldown_2: Label = $Camera2D/UIRoot/Cooldown2
@@ -79,3 +79,7 @@ func _get_time() -> String:
 func _leveling_up() -> void:
 	get_tree().paused = true
 	level_up.show()
+
+# Level has been won, Switch to win screen
+func _on_level_timer_timeout() -> void:
+	pass # Replace with function body.
