66cb4898fff9a11f8ec667219f6804dd800d3182
Max Mäkinen <quantumhound@gmail.com>
2024-06-03 10:54:43 +0300

    Added debugscreen to show data

---
+++ debugsceen.gd
@@ -0,0 +1,17 @@
+extends Control
+
+@onready var max_health: Label = $ColorRect/MaxHealth
+@onready var move_mult: Label = $ColorRect/MoveMult
+@onready var damage_mult: Label = $ColorRect/DamageMult
+@onready var health_mult: Label = $ColorRect/HealthMult
+@onready var move_speed: Label = $ColorRect/MoveSpeed
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	max_health.text = "Max health : " + str(PlayerData.get_max_health())
+	health_mult.text = "Health mult : " + str(PlayerData.get_health_mult())
+	move_speed.text = "MoveSpeed : " + str(PlayerData.get_move_speed())
+	move_mult.text = "Move mult : " + str(PlayerData.get_move_mult())
+	damage_mult.text = "Damage mult : " + str(PlayerData.get_atk_dmg_mult())
